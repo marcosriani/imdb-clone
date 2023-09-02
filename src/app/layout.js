6c7daa840cd3427497e830488a8771329ca40c2f
MarcosCabrini Riani_dos_Reis <marcosriani@hotmail.com>
2023-09-02 15:45:44 +0100

    Add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import './globals.css';
+import Providers from './Providers';
 
 export const metadata = {
   title: 'IMDb Clone',
@@ -13,14 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* Header */}
-        <Header />
+        <Providers>
+          {/* Header */}
+          <Header />
 
-        {/* Navbar */}
+          {/* Navbar */}
 
-        {/* SearchBox */}
+          {/* SearchBox */}
 
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   );
