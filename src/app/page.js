932bd585aa4cb2b310e7cba1599f6a7fcab8bc0e
MarcosCabrini Riani_dos_Reis <marcosriani@hotmail.com>
2023-09-03 16:22:16 +0100

    Add movie page

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
+  let page = 1;
 
   // Data will be cached in the server using revalidate
   const res = await fetch(
